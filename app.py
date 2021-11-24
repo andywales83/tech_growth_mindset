eb1bbfb78d4e0d8a167e2afff1aed21b891f43c3
Andrew Llewellyn <andrew.llewellyn83@gmail.com>
2021-11-24 20:52:58 +0000

    Add categories and topics dropdowns

--- app.py
+++ app.py
@@ -97,9 +97,13 @@ def logout():
     return redirect(url_for("login"))
 
 
+# ---------- Resource CRUD Functionality ---------- #
 @app.route("/add_resource")
 def add_resource():
-    return render_template("add_resource.html")
+    categories = mongo.db.categories.find().sort("category_name", 1)
+    topics = mongo.db.topics.find().sort("topic_name", 1)
+    return render_template("add_resource.html", categories=categories,
+                           topics=topics)
 
 
 # ---------- Profile Functionality ---------- #
